8e9f8593cb851fd56c227320c938792f4425d9c1
蘿蔔 <55518831+iambjlu@users.noreply.github.com>
2025-06-07 10:07:51 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+import subprocess
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    output = ""
+    if request.method == "POST":
+        command = request.form.get("command")
+        try:
+            output = command+("\n\n")+subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT, universal_newlines=True)
+        except subprocess.CalledProcessError as e:
+            output = e.output
+    return render_template("index.html", output=output)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8001)
